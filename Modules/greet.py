d8ddeaf797489e1173bf2b26dd0e5be428b227d4
Bisma Batool <bismabatool.1409@gmail.com>
2026-08-03 14:04:31 +0500

    Add welcome and goodbye functions in greet.py

---
+++ Modules/greet.py
@@ -0,0 +1,9 @@
+def welcome(name):
+    print("\n================================")
+    print(f"Welcome, {name}!")
+    print("================================")
+
+
+def goodbye(name):
+    print("\nThank you for using the calculator.")
+    print(f"Goodbye, {name}! Have a great day!")
